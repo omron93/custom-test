3acc980bfebb658b68d10973173be74cd1210440
Marek Skalicky <mskalicky@seznam.cz>
2019-02-13 08:09:29 +0000

    Create table is not exists on every questionnaire submition

--- send.php
+++ send.php
@@ -4,6 +4,33 @@ ini_set("default_charset", "UTF-8");
 
 <?php
 include "./connect.php";/* připojení k databázi */
+
+$table1 = "CREATE TABLE IF NOT EXISTS response_sessions (
+  id_session int(10) NOT NULL AUTO_INCREMENT,
+  start varchar(40) NOT NULL,
+  end varchar(40) NOT NULL,
+  custom_vars LONGTEXT,
+  custom_data LONGTEXT,
+  PRIMARY KEY (id_session)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1";
+
+$table2 = "CREATE TABLE IF NOT EXISTS response_answers (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  id_session int(10) NOT NULL,
+  question_id int(10) NOT NULL,
+  time varchar(40) NOT NULL,
+  answer varchar(40) NOT NULL,
+  PRIMARY KEY (id),
+  KEY id_session (id_session)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1";
+
+if ($mysqli->query($table1) === TRUE && $mysqli->query($table2) === TRUE) {
+    error_log("Database created successfully");
+} else {
+    error_log("Error creating database: " . $mysqli->error);
+}
+
+
 $vars = $_POST["custom_vars"];
 $data = $_POST["custom_data"];
 $start = $_POST["start"];
