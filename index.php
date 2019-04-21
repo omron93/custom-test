2e01ae64f458051c7d71cc0ecc6181902b71784c
Marek Skalicky <mskalicky@seznam.cz>
2019-04-21 10:42:35 +0100

    Use correct HTML5 header

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (file_exists($cache_filename))
 ob_start();
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html>
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
